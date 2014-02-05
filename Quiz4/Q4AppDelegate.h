77de66adb3924945bfd8b15624210ded841816fe
Michael Toth <michael@virtualpianist.com>
2014-02-05 09:31:54 -0500

    Initial Commit

---
+++ Quiz4/Q4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Q4AppDelegate.h
+//  Quiz4
+//
+//  Created by Michael Toth on 2/5/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Q4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz4
+//
+//  Created by Michael Toth on 2/5/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Q4AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Q4AppDelegate class]));
+    }
+}
